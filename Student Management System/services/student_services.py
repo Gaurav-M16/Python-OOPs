a8889314c93713aaf1f5f363e569c4e592c76db4
Gaurav Methe <gaurav.methe16@gmail.com>
2026-08-03 21:23:33 +0530

    added service file

---
+++ Student Management System/services/student_services.py
@@ -0,0 +1,10 @@
+class StudentService:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, students):
+        self.student.append(students)
+
+    def display_student(self):
+        for student in self.students:
+            print(student)
