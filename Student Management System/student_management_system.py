77fd47a503a778eb17d09f852f8748962bdbde13
Gaurav Methe <gaurav.methe16@gmail.com>
2026-08-03 19:50:56 +0530

    added spaces

--- Student Management System/student_management_system.py
+++ Student Management System/student_management_system.py
@@ -5,9 +5,11 @@ class Student:
         self.roll_no = roll_no
 
     def show_details(self):
-        print("Student Information")
+        print("\nStudent Information")
         print("-"*20)
-        print(f"Name: {self.name}, \nAge: {self.age}, \nRoll no: {self.roll_no}")
+        print(f"Name    : {self.name} \
+              \nAge     : {self.age} \
+              \nRoll no : {self.roll_no}")
 
-obj = Student("gaurav", 21, 2016)
+obj = Student("Gaurav", 21, 2016)
 obj.show_details()
