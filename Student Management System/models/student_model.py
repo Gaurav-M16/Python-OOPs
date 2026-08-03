ce677582d0522eadd082d15a255250269a478d80
Gaurav Methe <gaurav.methe16@gmail.com>
2026-08-03 21:18:25 +0530

    created model setup

---
+++ Student Management System/models/student_model.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, age, roll_no):
+            self.name = name
+            self.age = age
+            self.roll_no = roll_no
+    
+    def show_details(self):
+        print("\nStudent Information")
+        print("-"*20)
+        print(f"Name    : {self.name} \
+              \nAge     : {self.age} \
+              \nRoll no : {self.roll_no}")
